daec7cc213273a350e4eb49359809f5a5870fe61
BlackDaddy220232 <sashamynzul@gmail.com>
2024-03-17 23:24:14 +0300

    feat: Security Service

--- src/main/java/com/weather/weather/controller/SecurityController.java
+++ src/main/java/com/weather/weather/controller/SecurityController.java
@@ -8,6 +8,7 @@ import com.weather.weather.model.dto.SignUpRequest;
 import com.weather.weather.model.entity.Country;
 import com.weather.weather.security.JwtCore;
 import com.weather.weather.model.entity.User;
+import com.weather.weather.service.SecurityService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,91 +26,22 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/auth")
 public class SecurityController {
-    private UserRepository userRepository;
-    private PasswordEncoder passwordEncoder;
-    private AuthenticationManager authenticationManager;
-    private CountryRepository countryRepository;
-    private JwtCore jwtCore;
-
+    private SecurityService securityService;
     @Autowired
-    public void setUserRepository(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public void setSecurityService(SecurityService securityService) {
+        this.securityService = securityService;
     }
-
-    @Autowired
-    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
+    @PostMapping("/signup")
+    ResponseEntity<String> signup(@RequestBody SignUpRequest signUpRequest) {
+        return ResponseEntity.ok(securityService.register(signUpRequest));
     }
-
-    @Autowired
-    public void setAuthenticationManager(AuthenticationManager authenticationManager) {
-        this.authenticationManager = authenticationManager;
-    }
-    @Autowired
-    public void setCountyRepository(CountryRepository countyRepository){
-        this.countryRepository=countyRepository;
-    }
-
-    @Autowired
-    public void setJwtCore(JwtCore jwtCore) {
-        this.jwtCore = jwtCore;
-    }
-
-@PostMapping("/signup")
-ResponseEntity<String> signup(@RequestBody SignUpRequest signUpRequest) {
-    if (userRepository.existsUserByUsername(signUpRequest.getUsername()).booleanValue()) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("This nickname already exists");
-    }
-
-    String countryName = signUpRequest.getCountry();
-    Country country = countryRepository.findCountryByCountryName(countryName);
-    if (country == null) {
-        // Если страна не существует, создаем новую страну
-        country = new Country();
-        country.setCountryName(countryName);
-        countryRepository.save(country);
-    }
-
-    User user = new User();
-    user.setUsername(signUpRequest.getUsername());
-    user.setPassword(passwordEncoder.encode(signUpRequest.getPassword()));
-    user.setCountry(country);
-    user.setRole("ROLE_USER");
-    userRepository.save(user);
-
-    return ResponseEntity.ok("Chinazes");
-}
-    @GetMapping("/signin")
-        public String signinShow(){
-            return "signin";
-        }
     @PostMapping("/signin")
     ResponseEntity<String> signin(@RequestBody SignInRequest signInRequest) {
-        Authentication authentication = null;
-        try {
-            authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(signInRequest.getUsername(), signInRequest.getPassword()));
-        } catch (BadCredentialsException e) {
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-        }
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String jwt = jwtCore.generateToken(authentication);
-        return ResponseEntity.ok(jwt);
+        return ResponseEntity.ok(securityService.login(signInRequest));
     }
     @PatchMapping("/editPassword")
     public ResponseEntity<String> editPassword(@RequestBody PasswordRequest passwordRequest, HttpServletRequest request) {
-        String authorizationHeader = request.getHeader("Authorization");
-        String token;
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            token = authorizationHeader.substring(7);
-        }
-        else
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        String username = jwtCore.getNameFromJwt(token);
-        User user=userRepository.findUserByUsername(username).orElseThrow(()->new UsernameNotFoundException(
-                String.format("User '%s' not found",username)));
-        user.setPassword(passwordEncoder.encode(passwordRequest.getPassword()));
-        userRepository.save(user);
-
+        securityService.changePas(passwordRequest,request);
         return  ResponseEntity.ok("Password was successfully updated");
     }
 
--- src/main/java/com/weather/weather/service/SecurityService.java
+++ src/main/java/com/weather/weather/service/SecurityService.java
@@ -1,4 +1,103 @@
 package com.weather.weather.service;
 
+import com.weather.weather.dao.CountryRepository;
+import com.weather.weather.dao.UserRepository;
+import com.weather.weather.model.dto.PasswordRequest;
+import com.weather.weather.model.dto.SignInRequest;
+import com.weather.weather.model.dto.SignUpRequest;
+import com.weather.weather.model.entity.Country;
+import com.weather.weather.model.entity.User;
+import com.weather.weather.security.JwtCore;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+@Transactional
 public class SecurityService {
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+    private AuthenticationManager authenticationManager;
+    private JwtCore jwtCore;
+
+    private CountryRepository countryRepository;
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Autowired
+    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+    @Autowired
+    public void setAuthenticationManager(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Autowired
+    public void setJwtCore(JwtCore jwtCore) {
+        this.jwtCore = jwtCore;
+    }
+
+    @Autowired
+    public void setCountyRepository(CountryRepository countyRepository){
+        this.countryRepository=countyRepository;
+    }
+
+    public String register(SignUpRequest signUpRequest){
+        if (userRepository.existsUserByUsername(signUpRequest.getUsername()).booleanValue()) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+        User user = new User();
+        user.setUsername(signUpRequest.getUsername());
+        user.setPassword(passwordEncoder.encode(signUpRequest.getPassword()));
+        Country country = countryRepository.findCountryByCountryName(signUpRequest.getCountry());
+        if (country == null) {
+            country = new Country();
+            country.setCountryName(signUpRequest.getCountry());
+            countryRepository.save(country);
+        }
+        user.setCountry(country);
+        user.setRole("ROLE_USER");
+        userRepository.save(user);
+        Authentication authentication = null;
+        authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(signUpRequest.getUsername(), signUpRequest.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return jwtCore.generateToken(authentication);
+    }
+
+    public String login(SignInRequest signInRequest){
+        Authentication authentication = null;
+        try {
+            authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(signInRequest.getUsername(), signInRequest.getPassword()));
+        } catch (BadCredentialsException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return(jwtCore.generateToken(authentication));
+    }
+    public void changePas(PasswordRequest passwordRequest, HttpServletRequest request){
+        String authorizationHeader = request.getHeader("Authorization");
+        String token;
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            token = authorizationHeader.substring(7);
+        }
+        else
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        String username = jwtCore.getNameFromJwt(token);
+        User user=userRepository.findUserByUsername(username).orElseThrow(()->new UsernameNotFoundException(
+                String.format("User '%s' not found",username)));
+        user.setPassword(passwordEncoder.encode(passwordRequest.getPassword()));
+        userRepository.save(user);
+    }
 }
