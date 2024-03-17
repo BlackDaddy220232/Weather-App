5a1e6a63e56da0e3ec9d17cbb1bbc873a9555046
BlackDaddy220232 <sashamynzul@gmail.com>
2024-03-17 23:29:39 +0300

    fix: bugs

--- src/main/java/com/weather/weather/controller/SecurityController.java
+++ src/main/java/com/weather/weather/controller/SecurityController.java
@@ -1,25 +1,12 @@
 package com.weather.weather.controller;
 
-import com.weather.weather.dao.CountryRepository;
-import com.weather.weather.dao.UserRepository;
 import com.weather.weather.model.dto.PasswordRequest;
 import com.weather.weather.model.dto.SignInRequest;
 import com.weather.weather.model.dto.SignUpRequest;
-import com.weather.weather.model.entity.Country;
-import com.weather.weather.security.JwtCore;
-import com.weather.weather.model.entity.User;
 import com.weather.weather.service.SecurityService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,8 +20,14 @@ public class SecurityController {
     }
     @PostMapping("/signup")
     ResponseEntity<String> signup(@RequestBody SignUpRequest signUpRequest) {
-        return ResponseEntity.ok(securityService.register(signUpRequest));
+        try {
+            return ResponseEntity.ok(securityService.register(signUpRequest));
+        }
+        catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
+
     @PostMapping("/signin")
     ResponseEntity<String> signin(@RequestBody SignInRequest signInRequest) {
         return ResponseEntity.ok(securityService.login(signInRequest));
